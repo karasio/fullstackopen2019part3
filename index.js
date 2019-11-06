a2823a547cc132ab59764d495d43da709b9155be
Katri Rasio <katri.rasio@metropolia.fi>
2019-11-06 10:45:53 +0200

    small changes

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ let persons = [
   }
 ];
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 app.get('/info', (request, response) => {
   const date = new Date();
@@ -108,7 +108,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
